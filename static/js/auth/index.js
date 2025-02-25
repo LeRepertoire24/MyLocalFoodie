3c6b901ffed6d01bd90e6a7b1c24aa2c13f41fc0
LeRepertoire24 <LeRepertoire24@gmail.com>
2025-02-25 16:18:58 +1100

    Create index.js

---
+++ static/js/auth/index.js
@@ -0,0 +1,23 @@
+// static/js/auth/index.js
+
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.classList.add("hidden");
+}
+
+function showLoadingOverlay(show) {
+  const overlay = document.getElementById("loading-overlay");
+  if (show) {
+    overlay.classList.remove("hidden");
+  } else {
+    overlay.classList.add("hidden");
+  }
+}
+
+/**
+ * Retrieve CSRF token from meta tag (Flask-WTF places it there)
+ */
+function getCSRFToken() {
+  const metaTag = document.querySelector("meta[name='csrf-token']");
+  return metaTag ? metaTag.getAttribute("content") : "";
+}
